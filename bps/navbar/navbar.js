4d9916326b62d206e0f17767551c317c8a0422ff
toocels <toocels@gmail.com>
2023-06-08 20:16:35 +0530

    added SIMPLE readme

--- bps/navbar/navbar.js
+++ bps/navbar/navbar.js
@@ -1,30 +1,3 @@
-function includeHTML() {
-	var z, i, elmnt, file, xhttp;
-	z = document.getElementsByTagName("*");
-	for (i = 0; i < z.length; i++) {
-		elmnt = z[i];
-		file = elmnt.getAttribute("w3-include-html");
-		if (file) {
-			xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4) {
-					if (this.status == 200) {
-						elmnt.innerHTML = this.responseText;
-					}
-					if (this.status == 404) {
-						elmnt.innerHTML = "Page not found.";
-					}
-					elmnt.removeAttribute("w3-include-html");
-					includeHTML();
-				}
-			}
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			return;
-		}
-	}
-}
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
 	var currentScrollPos = window.pageYOffset;
