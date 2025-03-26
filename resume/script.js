ddcbdb3c5a924dd372c979a4f4d7a5add19fc9d5
toocels <toocels@gmail.com>
2025-03-26 09:41:36 +0530

    Better

--- resume/script.js
+++ resume/script.js
@@ -4,25 +4,8 @@ window.addEventListener("beforeprint", function() {
 	var elementsToHide = document.querySelectorAll("#footer, #navbar, h1, button, .top");
 	document.querySelector("main").style.width = "1200px"
 	document.querySelector(".top").style.width = "1200px"
+	document.title = 'Resume-PranavKumar'
 	elementsToHide.forEach(function(element) {
 		element.style.display = "none";
 	});
 });
-
-function setSize() {
-	return
-	if (window.innerWidth < 1200) {
-		document.querySelector("main").style.width = (window.innerWidth - 30) + "px"
-		document.querySelector(".top").style.width = (window.innerWidth - 30) + "px"
-	} else if (screen.width < 500) {
-		document.querySelector("main").style.width = (window.innerWidth - 30) + "px"
-		document.querySelector(".top").style.width = (window.innerWidth - 30) + "px"
-	}
-}
-
-
-setSize()
-
-window.addEventListener("resize", () => {
-	setSize()
-})
