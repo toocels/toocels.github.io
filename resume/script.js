805b7da5daf3edfa1f0db24f58ff7ed4ed6507c9
toocels <toocels@gmail.com>
2026-03-07 15:55:51 +0530

    smol

--- resume/script.js
+++ resume/script.js
@@ -1,11 +1,17 @@
-console.log("Wassup inspect element hackers.")
-
-window.addEventListener("beforeprint", function() {
-	var elementsToHide = document.querySelectorAll("#footer, #navbar, h1, button, .top");
-	document.querySelector("main").style.width = "1200px"
-	document.querySelector(".top").style.width = "1200px"
-	document.title = '126157041_PranavKumar'
-	elementsToHide.forEach(function(element) {
-		element.style.display = "none";
-	});
+console.log("Wassup inspect element hackers.");
+window.addEventListener("beforeprint", function () {
+  var elementsToHide = document.querySelectorAll("#footer, #navbar, .top");
+  document.querySelector("main").style.width = "1200px";
+  document.title = "PranavKumarS";
+  elementsToHide.forEach(function (element) {
+    element.style.display = "none";
+  });
+});
+window.addEventListener("afterprint", function () {
+  var elementsToHide = document.querySelectorAll("#footer, #navbar, .top");
+  document.querySelector("main").style.width = "";
+  document.title = "PranavKumarS";
+  elementsToHide.forEach(function (element) {
+    element.style.display = "";
+  });
 });
